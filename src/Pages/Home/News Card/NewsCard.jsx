ce43a796c4e8faddfa9890d8e6976f12fe08d737
SQLAdmin <abuyeahia24@gmail.com>
2023-04-24 05:02:58 +0600

    added ui to middle side

--- src/Pages/Home/News Card/NewsCard.jsx
+++ src/Pages/Home/News Card/NewsCard.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 /* eslint-disable no-unused-vars */
-const NewsCard = ({news}) => {
-    const {_id,title,details}= news
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+const NewsCard = ({ news }) => {
+    const { _id, title, details, image_url, author } = news
     return (
-        <div>
-            
-        </div>
+        <Card className=" mb-4">
+            <Card.Header>
+            <Image  style={{height: '40px'}} src={author?.img} roundedCircle />
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text>
+                    {details.length < 250 ? <>{details}</> :
+                     <>{details.slice(0, 250)}...<Link to={`/news/${_id}`}>Read More</Link> </>}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
     );
 };
 
