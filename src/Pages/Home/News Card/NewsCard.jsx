5bb3a410b50dc90eb0260cbb53fdc833f224eb12
SQLAdmin <abuyeahia24@gmail.com>
2023-04-24 12:29:28 +0600

    middle section ui fixed

--- src/Pages/Home/News Card/NewsCard.jsx
+++ src/Pages/Home/News Card/NewsCard.jsx
@@ -1,15 +1,26 @@
+import moment from 'moment/moment';
 import React from 'react';
 import { Image } from 'react-bootstrap';
 /* eslint-disable no-unused-vars */
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 const NewsCard = ({ news }) => {
     const { _id, title, details, image_url, author } = news
     return (
         <Card className=" mb-4">
-            <Card.Header>
+            <Card.Header className='d-flex align-items-center'>
             <Image  style={{height: '40px'}} src={author?.img} roundedCircle />
+           
+           <div className='ps-2  flex-grow-1'>
+            <p className='mb-0'>{author?.name}</p>
+            <p><small>{moment(author.published_date).format('yyyy-MM-DD')}</small></p>
+           </div>
+           <div>
+        <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+           </div>
+
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
