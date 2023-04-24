7c6861d4fc0f5a0c8133f0379cd9a4b48fa154e3
SQLAdmin <abuyeahia24@gmail.com>
2023-04-24 12:54:24 +0600

    fix ui design of rating

--- src/Pages/Home/News Card/NewsCard.jsx
+++ src/Pages/Home/News Card/NewsCard.jsx
@@ -5,21 +5,22 @@ import { Image } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
+import { FaRegBookmark, FaShareAlt, FaEye, FaRegStar, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 const NewsCard = ({ news }) => {
-    const { _id, title, details, image_url, author } = news
+    const { _id, title, details, image_url, author, rating, total_view } = news
     return (
         <Card className=" mb-4">
             <Card.Header className='d-flex align-items-center'>
-            <Image  style={{height: '40px'}} src={author?.img} roundedCircle />
-           
-           <div className='ps-2  flex-grow-1'>
-            <p className='mb-0'>{author?.name}</p>
-            <p><small>{moment(author.published_date).format('yyyy-MM-DD')}</small></p>
-           </div>
-           <div>
-        <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
-           </div>
+                <Image style={{ height: '40px' }} src={author?.img} roundedCircle />
+
+                <div className='ps-2  flex-grow-1'>
+                    <p className='mb-0'>{author?.name}</p>
+                    <p><small>{moment(author.published_date).format('yyyy-MM-DD')}</small></p>
+                </div>
+                <div>
+                    <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+                </div>
 
             </Card.Header>
             <Card.Body>
@@ -27,11 +28,29 @@ const NewsCard = ({ news }) => {
                 <Card.Img variant="top" src={image_url} />
                 <Card.Text>
                     {details.length < 250 ? <>{details}</> :
-                     <>{details.slice(0, 250)}...<Link to={`/news/${_id}`}>Read More</Link> </>}
+                        <>{details.slice(0, 250)}...<Link to={`/news/${_id}`}>Read More</Link> </>}
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-lg-flex">
+                <div className='flex-grow-1'>
+                    <Rating
+
+                        placeholderRating={rating.number}
+                        readonly
+                        emptySymbol={<FaRegStar></FaRegStar>}
+                        placeholderSymbol={<FaStar className='text-warning'></FaStar>}
+                        fullSymbol={<FaStar></FaStar>}
+
+                    >
+
+                    </Rating>
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaEye></FaEye> {total_view}
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
